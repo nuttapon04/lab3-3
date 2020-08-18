b73f3a76863457af8d29735e325c97e4a5c2347d
Nuttapon juabsawat <Nuttaponjuabsawattt@gmail.com>
2020-08-18 02:20:41 +0000

    lab3-3

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main(void) {
-  printf("Hello World\n");
+int main(void)
+{
+  float a, b;
+  printf("Enter A : ");
+  scanf("%f", &a);
+  printf("Enter B : ");
+  scanf("%f", &b);
+  printf("%0.2f * %0.2f = %0.2f", a, b, a*b);
   return 0;
 }
+
